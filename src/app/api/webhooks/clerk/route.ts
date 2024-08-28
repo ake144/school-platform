99c021f8ecb1382cbeacb358c7a73186932922c8
ake <tamiratake@gmail.com>
2024-08-29 01:02:00 +0300

    get request

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function POST(req:NextRequest, res:NextResponse) {
+export function GET(req:NextRequest, res:NextResponse) {
  return  'Hello World'
 
 }
