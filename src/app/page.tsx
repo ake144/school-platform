38a7e60e85c166c21b570338d192971518fe03c7
ake <tamiratake@gmail.com>
2024-08-30 08:51:22 +0300

    the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,46 +13,6 @@ const Homepage = () => {
   const [isLoading, setIsLoading] = useState(false); // Loading state to manage server updates
 
 
-  
-  const [userData, setUserData] = useState<any >(null);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      if (user) {
-        const fetchedUser = await getByClerkId(user.id);
-       if(fetchedUser){
-
-       
-        setUserData(fetchedUser);
- 
-       }
-
-      }
-    };
-
-    fetchPosts();
-  }, [user]);
-
-
-  const handleRoleChange = async (newRole:string) => {
-    setRole(newRole);
-    if (!user || !user.id) return;
-
-    try {
-      setIsLoading(true);
-      // Update the role in the database using the server action
-      await UpdateUser({ id: user.id, data: { role: newRole, name:userData.name, email:userData.email} });
-
-
-      alert(`Role updated to ${newRole}`);
-    } catch (error) {
-      console.error("Error updating user role:", error);
-      alert("Failed to update role. Please try again.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
 
 
   return (
@@ -65,23 +25,6 @@ const Homepage = () => {
           <div>
             <p>You are signed in as {user.fullName}</p>
 
-            <div className="flex flex-col items-center mt-5">
-              <label className="mb-2">Select Your Role:</label>
-              <select
-                value={role}
-                onChange={(e) => handleRoleChange(e.target.value)}
-                className="p-2 border border-gray-300 rounded-md"
-                disabled={isLoading}
-              >
-                <option value="" disabled>
-                  Select role
-                </option>
-                <option value="Student">Student</option>
-                <option value="Teacher">Teacher</option>
-                <option value="Admin">Admin</option>
-                <option value="Parent">Parent</option>
-              </select>
-            </div>
 
             <button
               onClick={()=>router.push('/admin')}
